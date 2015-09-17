7e1c6ed30bdf61f3d1ae8963317b0429d9075b43
Sven Lehnert <mail@sven-lehnert.de>
2015-09-17 21:51:15 +0200

    Yes ;) I'm able to display ACF Form elements and save the data. First step done

---
+++ includes/form-elements.php
@@ -0,0 +1,172 @@
+<?php
+
+function buddyforms_acf_admin_settings_sidebar_metabox($form, $selected_form_slug){
+
+    $form->addElement(new Element_HTML('
+		<div class="accordion-group postbox">
+			<div class="accordion-heading"><p class="accordion-toggle" data-toggle="collapse" data-parent="#accordion_'.$selected_form_slug.'" href="#accordion_'.$selected_form_slug.'_acf_options">Advanced Custom Fields</p></div>
+		    <div id="accordion_'.$selected_form_slug.'_acf_options" class="accordion-body collapse">
+				<div class="accordion-inner">'));
+
+
+    $form->addElement(new Element_HTML('<p><a href="ACF/'.$selected_form_slug.'" class="action">ACF Group</a></p>'));
+    //$form->addElement(new Element_HTML('<p><a href="ACF-Field/'.$selected_form_slug.'" class="action">ACF Field</a></p>'));
+
+
+
+$group_ID = 92;
+
+$fields = array();
+$fields = apply_filters('acf/field_group/get_fields', $fields, $group_ID);
+
+    echo '<pre>';
+    print_r($fields);
+    echo '</pre>';
+
+//if( $fields )
+//{
+//    foreach( $fields as $field )
+//    {
+//        $value = get_field( $field['name'] );
+//
+//        echo '<dl>';
+//        echo '<dt>' . $field['label'] . '</dt>';
+//        echo '<dd>' .$value . '</dd>';
+//        echo '</dl>';
+//    }
+//}
+
+
+
+
+    $form->addElement(new Element_HTML('
+				</div>
+			</div>
+		</div>'));
+
+    return $form;
+}
+add_filter('buddyforms_admin_settings_sidebar_metabox','buddyforms_acf_admin_settings_sidebar_metabox',1,2);
+
+/*
+ * Create the new Form Builder Form Element
+ *
+ */
+function bf_acf_create_new_form_builder_form_element($form_fields, $form_slug, $field_type, $field_id){
+    global $field_position;
+    $buddyforms_options = get_option('buddyforms_options');
+
+    switch ($field_type) {
+
+        case 'ACF':
+            unset($form_fields);
+            $name = 'ACF';
+
+            // get acf's
+            $posts = get_posts(array(
+                'numberposts' 	=> -1,
+                'post_type' 	=> 'acf',
+                'orderby' 		=> 'menu_order title',
+                'order' 		=> 'asc',
+                'suppress_filters' => false,
+            ));
+
+            $acf_groups = Array();
+
+            if( $posts ){ foreach( $posts as $post ){
+
+                $acf_groups[$post->ID] = $post->post_title;
+
+            }}
+
+
+            $acf_group = 'false';
+            if (isset($buddyforms_options['buddyforms'][$form_slug]['form_fields'][$field_id]['acf_group']))
+                $acf_group = $buddyforms_options['buddyforms'][$form_slug]['form_fields'][$field_id]['acf_group'];
+            $form_fields['advanced']['acf_group']            = new Element_Checkbox('', "buddyforms_options[buddyforms][" . $form_slug . "][form_fields][" . $field_id . "][acf_group]", $acf_groups, array('value' => $acf_group));
+
+
+
+
+            if (isset($buddyforms_options['buddyforms'][$form_slug]['form_fields'][$field_id]['name']))
+                $name = $buddyforms_options['buddyforms'][$form_slug]['form_fields'][$field_id]['name'];
+            $form_fields['left']['name']        = new Element_Textbox('<b>' . __('Name', 'buddyforms') . '</b>', "buddyforms_options[buddyforms][" . $form_slug . "][form_fields][" . $field_id . "][name]", array('value' => $name));
+
+            $form_fields['right']['slug']		= new Element_Hidden("buddyforms_options[buddyforms][".$form_slug."][form_fields][".$field_id."][slug]", 'hierarchical');
+
+            $form_fields['right']['type']	    = new Element_Hidden("buddyforms_options[buddyforms][".$form_slug."][form_fields][".$field_id."][type]", $field_type);
+            $form_fields['right']['order']		= new Element_Hidden("buddyforms_options[buddyforms][".$form_slug."][form_fields][".$field_id."][order]", $field_position, array('id' => 'buddyforms/' . $form_slug .'/form_fields/'. $field_id .'/order'));
+            break;
+
+    }
+
+    return $form_fields;
+}
+add_filter('buddyforms_form_element_add_field','bf_acf_create_new_form_builder_form_element',1,5);
+
+/*
+ * Display the new Form Element in the Frontend Form
+ *
+ */
+function bf_acf_create_frontend_form_element($form, $form_args){
+    global $buddyforms;
+
+    extract($form_args);
+
+    print_r($form_args);
+
+    $post_type = $buddyforms['buddyforms'][$form_slug]['post_type'];
+
+    if(!$post_type)
+        return $form;
+
+    if(!isset($customfield['type']))
+        return $form;
+
+    switch ($customfield['type']) {
+        case 'ACF':
+
+            $new_post = array(
+                'post_id'            => $post_id == 0 ? 'new': $post_id, // Create a new post
+                // PUT IN YOUR OWN FIELD GROUP ID(s)
+                'field_groups'       => array($customfield['acf_group'][0]), // Create post field group ID(s)
+                'form'               => false,
+            );
+
+            ob_start();
+                acf_form( $new_post );
+            $acf_form = ob_get_clean();
+
+
+
+            $form->addElement(new Element_HTML($acf_form));
+
+            break;
+    }
+
+    return $form;
+}
+add_filter('buddyforms_create_edit_form_display_element','bf_acf_create_frontend_form_element',1,2);
+
+add_action('buddyforms_update_post_meta','buddyforms_acf_update_post_meta', 10, 2);
+
+function buddyforms_acf_update_post_meta($customfield, $post_id){
+    if( $customfield['type'] == 'ACF' ){
+
+        $group_ID = $customfield['acf_group'];
+
+        $fields = array();
+        $fields = apply_filters('acf/field_group/get_fields', $fields, $group_ID);
+
+        $fields_values = $_POST[ 'fields' ];
+
+        if( $fields ) {
+            foreach( $fields as $field ) {
+
+                if(isset($fields_values[$field['key']]))
+                    update_field( $field['key'], $fields_values[$field['key']], $post_id );
+            }
+        }
+    }
+}
+
---
+++ loader.php
@@ -0,0 +1,138 @@
+<?php
+/*
+ Plugin Name: BuddyForms Advanced Custom Fields
+ Plugin URI: http://buddyforms.com/downloads/buddyforms-advanced-custom-fields/
+ Description: Integrates the populare ACF Plugin with BuddyForms. Use all ACF Fields in your form like native BuddyForms Form Elements
+ Version: 0.1
+ Author: Sven Lehnert
+ Author URI: https://profiles.wordpress.org/svenl77
+ License: GPLv2 or later
+ Network: false
+
+ *****************************************************************************
+ *
+ * This script is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ****************************************************************************
+ */
+
+
+class BuddyFormsACF {
+
+    /**
+     * @var string
+     */
+    public $version = '0.1';
+
+    /**
+     * Initiate the class
+     *
+     * @package buddyforms acf
+     * @since 0.1
+     */
+    public function __construct() {
+
+        add_action( 'init'					, array($this, 'includes')						, 4, 1);
+        add_action( 'plugins_loaded'		, array($this, 'load_plugin_textdomain'));
+//        add_action( 'admin_enqueue_scripts'	, array($this, 'buddyforms_acf_admin_style')		, 1, 1);
+        add_action( 'buddyforms_front_js_css_enqueue'	, array($this, 'buddyforms_acf_front_js_css')			, 2, 1);
+
+        $this->load_constants();
+
+    }
+
+    /**
+     * Defines constants needed throughout the plugin.
+     *
+     * These constants can be overridden in bp-custom.php or wp-config.php.
+     *
+     * @package buddyforms_acf
+     * @since 0.1
+     */
+    public function load_constants() {
+
+        if (!defined('BUDDYFORMS_ACF_PLUGIN_URL'))
+            define('BUDDYFORMS_ACF_PLUGIN_URL', plugins_url('/',__FILE__));
+
+        if (!defined('BUDDYFORMS_ACF_INSTALL_PATH'))
+            define('BUDDYFORMS_ACF_INSTALL_PATH', dirname(__FILE__) . '/');
+
+        if (!defined('BUDDYFORMS_ACF_INCLUDES_PATH'))
+            define('BUDDYFORMS_ACF_INCLUDES_PATH', BUDDYFORMS_ACF_INSTALL_PATH . 'includes/');
+
+        if (!defined('BUDDYFORMS_ACF_TEMPLATE_PATH'))
+            define('BUDDYFORMS_ACF_TEMPLATE_PATH', BUDDYFORMS_ACF_INSTALL_PATH . 'templates/');
+
+    }
+
+    /**
+     * Include files needed by BuddyForms
+     *
+     * @package buddyforms_acf
+     * @since 0.1
+     */
+    public function includes() {
+
+        require_once( BUDDYFORMS_ACF_INCLUDES_PATH . 'form-elements.php' );
+
+//        if (is_admin()){
+//
+//            require_once( BUDDYFORMS_ACF_INCLUDES_PATH . '/admin/form-builder/form-builder.php');
+//
+//        }
+
+
+    }
+
+    /**
+     * Load the textdomain for the plugin
+     *
+     * @package buddyforms_acf
+     * @since 0.1
+     */
+    public function load_plugin_textdomain() {
+        load_plugin_textdomain('buddyforms', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+    }
+
+    /**
+     * Enqueue the needed CSS for the admin screen
+     *
+     * @package buddyforms_acf
+     * @since 0.1
+     */
+    function buddyforms_acf_admin_style($hook_suffix) {
+    }
+
+    /**
+     * Enqueue the needed JS for the admin screen
+     *
+     * @package buddyforms_acf
+     * @since 0.1
+     */
+    function buddyforms_acf_admin_js($hook_suffix) {
+    }
+
+    /**
+     * Enqueue the needed JS for the frontend
+     *
+     * @package buddyforms_acf
+     * @since 0.1
+     */
+    function buddyforms_acf_front_js_css() {
+    }
+}
+
+$GLOBALS['BuddyFormsACF'] = new BuddyFormsACF();
+
